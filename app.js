7dc9e1fda7cb9a7b4ffd618a29f40d94c4128bbc
Mohamabo <m.abouelkhair91@gmail.com>
2025-04-14 16:02:04 -0400

    Exercise Answered

---
+++ app.js
@@ -0,0 +1,45 @@
+// Quick Question 1
+
+new Set([1, 1, 2, 2, 3, 4])
+// Output: { 1, 2, 3, 4 }
+
+// Quick Question 2
+
+[...new Set("referee")].join("")
+// Output: "ref"
+
+// Quick Question 3
+
+let m = new Map();
+m.set([1,2,3], true);
+m.set([1, 2, 3], false);
+/* Output:
+  0: {Array(3) => true}
+  1: {Array(3) => false}
+*/
+
+// hasDuplicate
+
+const hasDuplicate = arr => new Set(arr).size !== arr.length;
+
+// vowelCount
+
+function isVowel(char) {
+  return 'aeiouAEIOU'.includes(char);
+}
+
+function vowelCount(str) {
+    const vowelMap = new Map();
+    for (let char of str) {
+        let lowerCaseChar = char.toLowerCase();
+        if (isVowel(lowerCaseChar)) {
+            if (vowelMap.has(lowerCaseChar)) {
+                vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1);
+            } else {
+                vowelMap.set(lowerCaseChar, 1);
+            }
+        }
+    }
+    return vowelMap;
+}
+
